6c2e25aa5ad19fa5b5873138a1b1180a14bb56bf
chwaqaskrk@gmail.com <chwaqaskrk@gmail.com>
2021-08-25 12:54:08 +0500

    referrals settings page update

--- resources/views/admin/referral/index.blade.php
+++ resources/views/admin/referral/index.blade.php
@@ -5,12 +5,11 @@ Create referral
 @push('style')
     <style>
         div#refdiv {
-            border-bottom: 0 0 0 0.1 gray;
+            border-bottom: 2px solid rgba(0,0,0,0.3);
             height: auto;
-            padding-bottom: 10px;
-            border-bottom: 2px solid gray;
+            padding-bottom: 20px;
         }
-        input#refbonus {
+        input#refLevels {
             max-width: 240px;
         }
     </style>
@@ -22,20 +21,18 @@ Create referral
         <div class="col-md-12">
             <div class="row">
                 <div class="col-md-12">
-                    <form action="">
                         <div class="gap">
-                            <h4 class="input-label mt-2">Referral level</h4>
+                            <h4 class="input-label mt-2">Referral Levels</h4>
                                 <div class="d-flex justify-content-between" id="refdiv">
-                                <input type="text" class="form-control bg-white round-10 border-0" name="refbonus" value="{{old('refbonus')}}" id="refbonus">
-                                <input type="submit" value="Create bonus" class="btn btn-blue round-10 border-0 text-white">
-                            </div>
+                                    <input type="text" class="form-control bg-white round-10 border-0" name="refLevels" id="refLevels">
+                                    <button type="button" class="btn btn-blue round-10 border-0 text-white px-5 createLevels"> CREATE </button>
+                                </div>
                                 @error('refbonus')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                         </div>
-                    </form>
                 </div>
             </div>
         </div>
@@ -46,53 +43,8 @@ Create referral
     <div class="row">
         <div class="col-md-12">
             <h1 class="text-bold mt-3 mb-3">Referral level bonus</h1>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="gap mt-3">
-                        {{-- <h4 class="input-label mt-2">Referral level</h4> --}}
-                            <div class="d-flex justify-content-between">
-                                <h4 class="input-label">Level 1 bonus</h4>
-                                <input type="text" class="form-control bg-white round-10 border-0" name="levels" id="refbonus">
-                        </div>
-                            @error('levels')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                    </div>
-                </div>
-            </div>
-                <div class="row">
-                <div class="col-md-12">
-                    <div class="gap mt-3">
-                        {{-- <h4 class="input-label mt-2">Referral level</h4> --}}
-                            <div class="d-flex justify-content-between">
-                                <h4 class="input-label">Level 2 bonus</h4>
-                                <input type="text" class="form-control bg-white round-10 border-0" name="levels" id="refbonus">
-                        </div>
-                            @error('levels')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                    </div>
-                </div>
-            </div>
-                <div class="row">
-                <div class="col-md-12">
-                    <div class="gap mt-3">
-                        {{-- <h4 class="input-label mt-2">Referral level</h4> --}}
-                            <div class="d-flex justify-content-between">
-                                <h4 class="input-label">Level 3 bonus</h4>
-                                <input type="text" class="form-control bg-white round-10 border-0" name="levels" id="refbonus">
-                        </div>
-                            @error('levels')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                    </div>
-                </div>
+            <div class="ref-bonuses">
+
             </div>
         </div>
     </div>
@@ -100,4 +52,24 @@ Create referral
 </div>
 <!-- /.container-fluid -->
 @endsection
+@push('script')
+    <script>
+        $("#refLevels").on("keypress keyup blur",function (event) {
+            $(this).val($(this).val().replace(/[^\d].+/, ""));
+            if ((event.which < 48 || event.which > 57)) {
+                event.preventDefault();
+            }
+        });
+        $('.createLevels').click(function(){
+            if($('#refLevels').val()<=100){
+                $('.ref-bonuses').html("");
+                for(var i=1; i<=$('#refLevels').val(); i++){
+                    $('.ref-bonuses').append(" <div class='row'><div class='col-md-12'><div class='gap mt-3'><div class='d-flex justify-content-between'><h4 class='input-label'>Level "+i+" bonus</h4> <input type='text' class='form-control bg-white round-10 border-0' name='bonuses[]'></div></div></div></div>")
+                }
+            }else{
+                alert('too large value')
+            }
 
+        });
+    </script>
+@endpush
