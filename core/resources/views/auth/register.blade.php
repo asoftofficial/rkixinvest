b94fe792d8ab86864755e8c44ef3991a0c80b3df
azeemshahid850 <azeemshahid850@gmail.com>
2021-09-17 16:42:11 +0500

    changes fixed

--- core/resources/views/auth/register.blade.php
+++ core/resources/views/auth/register.blade.php
@@ -93,7 +93,7 @@ $("#username").on({
         </div>
         <div class="row">
             <div class="col-md-6 mt-2">
-                <label for="Country" class="text-dark">Country</label>
+                <label for="Country" class="text-dark mb-1">Country</label>
                 <select name="country" required id="countries" class="form-control bg-light round-10 border-0 " style="height:10vh !important;">
                 @foreach ($countries as $item)
                 <option value="{{$item->name}}">{{$item->name}}</option>
